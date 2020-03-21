a41f80ea73f67d667c6abf85c752323adfd469ac
Carrie Wladis <carriewladis@gmail.com>
2020-03-21 11:00:51 -0400

    create-react-app and first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './static/imgs/logo.svg';
+import './static/styles/App.css';
 
 function App() {
   return (
